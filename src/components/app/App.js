be4c67f2f540e39454a141cfee22b54877a780b7
Samjzwk <jozwiaksamuel@gmail.com>
2020-09-23 11:22:49 +0200

    Init Exo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,27 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import loadable from "@loadable/component";
-import { Dot } from "../Spinner";
-import {ThemeProvider} from '../../utils/context/ThemeContexte';
 
-//ici nous faisons du code splitting avec une interface de supply, il permet ici de charger uniquement ce dont il a besoin
-
-const fallBack = { fallback: <Dot /> };
-const Home = loadable(() => import("../../routes/Home"), fallBack);
-const Recipes = loadable(() => import("../../routes/Recipes"), fallBack);
-const notFound = loadable(() => import("../../routes/notFound"), fallBack);
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <ThemeProvider>
-          <Route path="/recipes" component={Recipes} />
-        </ThemeProvider>
-        <Route component={notFound} />
-      </Switch>
-    </Router>
+   <p>Hello</p>
   );
 }
 
